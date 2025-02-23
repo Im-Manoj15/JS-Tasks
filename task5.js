9b094182f96161910754dfadd96af0caa2e6a541
Im-Manoj15 <manojmanickam1595@gmail.com>
2025-02-23 23:04:50 +0530

    Create task5.js

---
+++ task5.js
@@ -0,0 +1,8 @@
+Question 5: Find the Maximum Number in an Array
+Problem: Write a function that finds the maximum number in an array.
+Input: [1, 3, 2, 8, 5]
+Output: 8
+
+Answer:
+const a = [1, 3, 2, 8, 5];
+console.log(Math.max(...a));
